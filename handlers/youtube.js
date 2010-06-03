92bc82b3c6b5c3b3fa5bfccb2cc0e5bed02441ce
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2010-06-03 20:14:22 +0200

    Handler loading code.
    Moved YouTube handler to its own file.
    Now every handler should live in the handlers
    directory (with a good name). Each handler will
    be a function which returns the handler itself.
    The register code of VideoHandlers will invoke
    that function and store the handler in the
    handlers array.
    The video tag creation have been moved to
    VideoHandlers object, and every handle can use
    it passing the DOM object to substitute, the
    video URL and some options.
    
    Thanks to Yaoha Chen, for providing some ideas
    implemented in this commit.

---
+++ handlers/youtube.js
@@ -0,0 +1,150 @@
+(function() {
+var YouTubeVideo = function(domObject, url) {
+  this.domObject = domObject;
+  this.url = url;
+};
+
+YouTubeVideo.youTubeRegEx = new RegExp('^http://www\.youtube\.com/v/');
+YouTubeVideo.canHandleURL = function(url) {
+  return YouTubeVideo.youTubeRegEx.test(url);
+};
+
+YouTubeVideo.prototype.videoIDRegEx = new RegExp('/([-_A-Z0-9]+)(&|$)', 'i');
+
+YouTubeVideo.oldSwfVarsSplitter = function(flashVarsRaw) {
+  var flashVars = {};
+  flashVarsRaw = flashVarsRaw.split(', ');
+  for (var idx = 0; idx < flashVarsRaw.length; idx++) {
+    var keyValues = flashVarsRaw[idx].split(': '),
+        key = keyValues.shift(),
+        val = keyValues.join(': ');
+    
+    if (key.charAt(0) == '"') {
+      key = key.substring(1, key.length - 1);
+    }
+    
+    if (val.charAt(0) == '"') {
+      val = val.substring(1, val.length - 1);
+    }
+    
+    flashVars[key] = val;
+  }
+  
+  return flashVars;
+};
+
+YouTubeVideo.newFlashvarsSplitter = function(flashVarsRaw) {
+  var flashVars = {};
+  flashVarsRaw = flashVarsRaw.split('&');
+  for (var idx = 0; idx < flashVarsRaw.length; idx++) {
+    var keyValues = flashVarsRaw[idx].split('='),
+        key = keyValues.shift(),
+        val = keyValues.join('=');
+    
+    flashVars[key] = val;
+  }
+  
+  return flashVars;
+};
+
+YouTubeVideo.prototype.swfVarsRegEx = [
+  [new RegExp('<param name=\\\\"flashvars\\\\" value=\\\\"(.*?)\\\\">'), YouTubeVideo.newFlashvarsSplitter],
+  [new RegExp("var swfArgs = \\{(.*?)\\}"), YouTubeVideo.oldSwfVarsSplitter],
+  [new RegExp("'SWF_ARGS': \\{(.*?)\\}"), YouTubeVideo.oldSwfVarsSplitter]
+];
+
+YouTubeVideo.prototype.watchURL = function() {
+  return "http://www.youtube.com/watch?v=" + this.videoID;
+};
+
+YouTubeVideo.prototype.videoSDURL = function() {
+  return "http://www.youtube.com/get_video?fmt=18&video_id=" + this.videoID + "&t=" + this.videoHash;
+};
+
+YouTubeVideo.prototype.videoHDURL = function() {
+  return "http://www.youtube.com/get_video?fmt=22&video_id=" + this.videoID + "&t=" + this.videoHash;
+};
+
+YouTubeVideo.prototype.start = function() {
+  this.videoID = this.videoIDRegEx.exec(this.url);
+  
+  if (this.videoID) {
+    this.videoID = this.videoID[1];
+    
+    var self = this;
+    chrome.extension.sendRequest({action: 'ajax',
+      args: {
+        type: 'GET',
+        url: this.watchURL()
+    }}, function() { return self.parseSwfVars.apply(self, arguments); });
+  }
+};
+
+YouTubeVideo.prototype.parseSwfVars = function(response) {
+  if (response.textStatus != 'success') {
+    return;
+  }
+  
+  var flashVarsRaw = null,
+      idx = 0;
+  for (idx = 0; flashVarsRaw == null && idx < this.swfVarsRegEx.length; idx++) {
+    flashVarsRaw = this.swfVarsRegEx[idx][0].exec(response.data);
+  }
+  
+  if (flashVarsRaw == null) {
+    return; // we haven't found the flashVars
+  }
+  idx -= 1; // idx gets increased before exiting the loop
+  
+  this.flashVars = this.swfVarsRegEx[idx][1](flashVarsRaw[1]);
+  this.videoHash = this.flashVars['t'];
+  
+  this.videoRequestStatus = [null, null];
+  
+  var self = this;
+  chrome.extension.sendRequest({action: 'ajax',
+    args: {
+      type: 'HEAD',
+      url: this.videoSDURL()
+  }}, function() { return self.handleSDVideoResponse.apply(self, arguments); });
+  
+  chrome.extension.sendRequest({action: 'ajax',
+    args: {
+      type: 'HEAD',
+      url: this.videoHDURL()
+  }}, function() { return self.handleHDVideoResponse.apply(self, arguments); });
+};
+
+YouTubeVideo.prototype.handleSDVideoResponse = function(response) {
+  if (response.textStatus == 'success') {
+    this.videoRequestStatus[0] = true;
+  } else if (response.textStatus == 'error' ||
+             response.textStatus == 'timeout') {
+    this.videoRequestStatus[0] = false;
+  }
+  
+  this.replaceFlashObjectWithVideo();
+};
+
+YouTubeVideo.prototype.handleHDVideoResponse = function(response) {
+  if (response.textStatus == 'success') {
+    this.videoRequestStatus[1] = true;
+  } else if (response.textStatus == 'error' ||
+             response.textStatus == 'timeout') {
+    this.videoRequestStatus[1] = false;
+  }
+  
+  this.replaceFlashObjectWithVideo();
+};
+
+YouTubeVideo.prototype.replaceFlashObjectWithVideo = function() {
+  if (this.videoRequestStatus[1] || this.videoRequestStatus[0]) {
+    var videoURL = this.videoRequestStatus[1] ? this.videoHDURL() : this.videoSDURL();
+    VideoHandlers.replaceFlashObjectWithVideo(this.domObject,
+      videoURL,
+      { watchURL: this.watchURL(), downloadURL: videoURL });
+  }
+};
+
+return YouTubeVideo;
+})
--- video5.js
+++ video5.js
@@ -1,171 +1,63 @@
-var YouTubeVideo = function(domObject, url) {
-  this.domObject = domObject;
-  this.url = url;
-};
-
-YouTubeVideo.youTubeRegEx = new RegExp('^http://www.youtube.com/v/');
-YouTubeVideo.canHandleURL = function(url) {
-  return YouTubeVideo.youTubeRegEx.test(url);
-};
-
-YouTubeVideo.prototype.videoIDRegEx = new RegExp('/([-_A-Z0-9]+)(&|$)', 'i');
-
-YouTubeVideo.oldSwfVarsSplitter = function(flashVarsRaw) {
-  var flashVars = {};
-  flashVarsRaw = flashVarsRaw.split(', ');
-  for (var idx = 0; idx < flashVarsRaw.length; idx++) {
-    var keyValues = flashVarsRaw[idx].split(': '),
-        key = keyValues.shift(),
-        val = keyValues.join(': ');
-    
-    if (key.charAt(0) == '"') {
-      key = key.substring(1, key.length - 1);
+var VideoHandlers = {
+  handlers: [],
+  
+  register: function() {
+    for (var idx in arguments) {
+      var url = chrome.extension.getURL('handlers/' + arguments[idx] + '.js');
+      chrome.extension.sendRequest({action: 'ajax',
+        args: {
+          type: 'GET',
+          url: url
+      }}, function(response) {
+        var handler = eval(response.data + '()');
+        if (handler) {
+          VideoHandlers.handlers.push(handler);
+        }
+      });
     }
-    
-    if (val.charAt(0) == '"') {
-      val = val.substring(1, val.length - 1);
+  },
+  
+  handleTagAndURL: function(obj, url) {
+    for (var idx = 0; idx < this.handlers.length; idx++) {
+      var videoHandler = this.handlers[idx];
+
+      if (videoHandler.canHandleURL(url)) {
+        var vh = new videoHandler(obj, url);
+        vh.start();
+        break;
+      }
     }
-    
-    flashVars[key] = val;
-  }
+  },
   
-  return flashVars;
-};
-
-YouTubeVideo.newFlashvarsSplitter = function(flashVarsRaw) {
-  var flashVars = {};
-  flashVarsRaw = flashVarsRaw.split('&');
-  for (var idx = 0; idx < flashVarsRaw.length; idx++) {
-    var keyValues = flashVarsRaw[idx].split('='),
-        key = keyValues.shift(),
-        val = keyValues.join('=');
-    
-    flashVars[key] = val;
-  }
-  
-  return flashVars;
-};
-
-YouTubeVideo.prototype.swfVarsRegEx = [
-  [new RegExp('<param name=\\\\"flashvars\\\\" value=\\\\"(.*?)\\\\">'), YouTubeVideo.newFlashvarsSplitter],
-  [new RegExp("var swfArgs = \\{(.*?)\\}"), YouTubeVideo.oldSwfVarsSplitter],
-  [new RegExp("'SWF_ARGS': \\{(.*?)\\}"), YouTubeVideo.oldSwfVarsSplitter]
-];
-
-YouTubeVideo.prototype.watchURL = function() {
-  return "http://www.youtube.com/watch?v=" + this.videoID;
-};
-
-YouTubeVideo.prototype.videoSDURL = function() {
-  return "http://www.youtube.com/get_video?fmt=18&video_id=" + this.videoID + "&t=" + this.videoHash;
-};
-
-YouTubeVideo.prototype.videoHDURL = function() {
-  return "http://www.youtube.com/get_video?fmt=22&video_id=" + this.videoID + "&t=" + this.videoHash;
-};
-
-YouTubeVideo.prototype.start = function(response) {
-  this.videoID = this.videoIDRegEx.exec(this.url);
-  
-  if (this.videoID) {
-    this.videoID = this.videoID[1];
-    console.log(this.videoID);
-    
-    var self = this;
-    chrome.extension.sendRequest({action: 'ajax',
-      args: {
-        type: 'GET',
-        url: this.watchURL()
-    }}, function() { return self.parseSwfVars.apply(self, arguments); });
-  }
-};
-
-YouTubeVideo.prototype.parseSwfVars = function(response) {
-  if (response.textStatus != 'success') {
-    return;
-  }
-  
-  var flashVarsRaw = null,
-      idx = 0;
-  for (idx = 0; flashVarsRaw == null && idx < this.swfVarsRegEx.length; idx++) {
-    flashVarsRaw = this.swfVarsRegEx[idx][0].exec(response.data);
-  }
-  
-  if (flashVarsRaw == null) {
-    console.log('SWF vars not found');
-    return; // we haven't found the flashVars
-  }
-  idx -= 1; // idx gets increased before exiting the loop
-  
-  this.flashVars = this.swfVarsRegEx[idx][1](flashVarsRaw[1]);
-  this.videoHash = this.flashVars['t'];
-  
-  this.videoRequestStatus = [null, null];
-  
-  var self = this;
-  chrome.extension.sendRequest({action: 'ajax',
-    args: {
-      type: 'HEAD',
-      url: this.videoSDURL()
-  }}, function() { return self.handleSDVideoResponse.apply(self, arguments); });
-  
-  chrome.extension.sendRequest({action: 'ajax',
-    args: {
-      type: 'HEAD',
-      url: this.videoHDURL()
-  }}, function() { return self.handleHDVideoResponse.apply(self, arguments); });
-};
-
-YouTubeVideo.prototype.handleSDVideoResponse = function(response) {
-  if (response.textStatus == 'success') {
-    this.videoRequestStatus[0] = true;
-  } else if (response.textStatus == 'error' ||
-             response.textStatus == 'timeout') {
-    this.videoRequestStatus[0] = false;
-  }
-  
-  this.replaceFlashObjectWithVideo();
-};
-
-YouTubeVideo.prototype.handleHDVideoResponse = function(response) {
-  if (response.textStatus == 'success') {
-    this.videoRequestStatus[1] = true;
-  } else if (response.textStatus == 'error' ||
-             response.textStatus == 'timeout') {
-    this.videoRequestStatus[1] = false;
-  }
-  
-  this.replaceFlashObjectWithVideo();
-};
-
-YouTubeVideo.prototype.replaceFlashObjectWithVideo = function() {
-  if (this.videoRequestStatus[1] || this.videoRequestStatus[0]) {
-    var videoUrl = this.videoRequestStatus[1] ? this.videoHDURL() : this.videoSDURL(),
-        videoTag = jQuery('<video controls="controls" width="100%">').attr(
-          'src', videoUrl),
-        wrapper = jQuery('<div class="v5-wrapper">'),
+  replaceFlashObjectWithVideo: function(domObject, videoUrl, options) {
+    var videoTag = jQuery('<video controls="controls" width="100%">').attr(
+                          'src', videoUrl),
+        wrapper  = jQuery('<div class="v5-wrapper">'),
         controls = jQuery('<div class="v5-controls">');
     wrapper.append(videoTag);
-    this.domObject.replaceWith(wrapper);
+    domObject.replaceWith(wrapper);
     wrapper.css({
       'width': videoTag.width(),
       'height': videoTag.height()
     });
-    
+
     videoTag.bind('loadedmetadata', function(e) {
       wrapper.css({
         'width': videoTag.width(),
         'height': videoTag.height()
       });
     });
-    
-    controls.append(jQuery('<a class="v5-goto" href="' + this.watchURL() + '">'));
-    controls.append(jQuery('<a class="v5-download" href="' + videoUrl + '">'));
+
+    if (options.watchURL) {
+      controls.append(jQuery('<a class="v5-goto" href="' + options.watchURL + '">'));
+    }
+    if (options.downloadURL) {
+      controls.append(jQuery('<a class="v5-download" href="' + options.downloadURL + '">'));
+    }
     wrapper.append(controls);
   }
 };
-
-var VideoHandlers = [YouTubeVideo];
+VideoHandlers.register('youtube');
 
 jQuery(window).bind('DOMNodeInserted', function(e) {
   jQuery('object, embed', e.target).each(function() {
@@ -223,7 +115,7 @@ function lookForFlashVideos(elem) {
 function handleEmbedTag(obj) {
   if (obj.attr('src') !== undefined &&
       obj.attr('type') == 'application/x-shockwave-flash') {
-    handleTagAndURL(obj, obj.attr('src'));
+    VideoHandlers.handleTagAndURL(obj, obj.attr('src'));
   }
 }
 
@@ -232,27 +124,16 @@ function handleObjectTag(obj) {
     if (obj.attr('data') !== undefined &&
         obj.attr('type') == 'application/x-shockwave-flash') {
       obj.attr('data-video5-visited', 'yes');
-      handleTagAndURL(obj, obj.attr('data'));
+      VideoHandlers.handleTagAndURL(obj, obj.attr('data'));
     } else {
       var children = obj.children('embed');
       if (children.length > 0 &&
           children.attr('src') !== undefined &&
           children.attr('type') == 'application/x-shockwave-flash') {
         obj.attr('data-video5-visited', 'yes');
-        handleTagAndURL(obj, children.attr('src'));
+        VideoHandlers.handleTagAndURL(obj, children.attr('src'));
       }
     }
   }
 }
 
-function handleTagAndURL(obj, url) {
-  for (var idx = 0; idx < VideoHandlers.length; idx++) {
-    var videoHandler = VideoHandlers[idx];
-    
-    if (videoHandler.canHandleURL(url)) {
-      var vh = new videoHandler(obj, url);
-      vh.start();
-      break;
-    }
-  }
-}
