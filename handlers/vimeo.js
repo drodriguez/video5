2a7d540c9bd74238ffd1b6290771b13daea96dde
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2010-07-17 19:27:56 +0200

    Handle Vimeo with Flash vars.
    For some reason seems like Vimeo is not providing
    access to the files anymore. I will keep the code,
    but probably we will have to disable it.

--- handlers/vimeo.js
+++ handlers/vimeo.js
@@ -1,30 +1,38 @@
 (function() {
-VimeoVideo = function(domObject, url) {
+VimeoVideo = function(domObject, url, flashVars) {
   this.domObject = domObject;
   this.url = url;
+  this.clip_id = flashVars.clip_id;
 };
 
 VimeoVideo.vimeoRegEx = new RegExp('^http://(?:www\\.)?vimeo\\.com/.+clip_id=(\\d+)');
 VimeoVideo.tryHandling = function(node, url) {
-  if (VimeoVideo.vimeoRegEx.test(url)) {
-    return new VimeoVideo(node, url);
+  var flashVars = FlashVarsParser.parse(node);
+  if (VimeoVideo.vimeoRegEx.test(url) ||
+      flashVars.clip_id !== undefined) {
+    return new VimeoVideo(node, url, flashVars);
   } else {
     return null;
   }
 };
 
 VimeoVideo.prototype.start = function() {
-  var match = VimeoVideo.vimeoRegEx.exec(this.url);
-  if (match) {
-    this.clipId = match[1];
-    this.watchUrl = 'http://vimeo.com/' + this.clipId;
-    
-    var videoUrl = 'http://vimeo.com/play_redirect?clip_id=' + this.clipId;
-
-    VideoHandlers.replaceFlashObjectWithVideo(this.domObject,
-      videoUrl,
-      { watchURL: this.watchUrl });
+  var match, clipId;
+  if (this.clip_id !== undefined) {
+    clipId = this.clip_id;
+  } else if ((match = VimeoVideo.vimeoRegEx.exec(this.url)) !== null) {
+    clipId = match[1];
+  } else {
+    return;
   }
+
+  this.watchUrl = 'http://vimeo.com/' + clipId;
+
+  var videoUrl = 'http://vimeo.com/play_redirect?clip_id=' + clipId;
+
+  VideoHandlers.replaceFlashObjectWithVideo(this.domObject,
+    videoUrl,
+    { watchURL: this.watchUrl });
 };
 
 return VimeoVideo;
